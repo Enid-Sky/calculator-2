e12bfd4c6dccd77a382b0bb217b5992cf7c81351
Enid-Sky <enid.sky@protonmail.com>
2020-09-30 18:50:04 -0700

    initialize while loop

--- calculator-2.py
+++ calculator-2.py
@@ -2,3 +2,13 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
+# Initialize loop
+while True:
+    # Tokenize a string
+    input_string = input("Enter your equation: ")
+    tokens = input_string.split(' ')
+    result = None
+
+    operator = tokens[0]
+    if operator == "q":
+        break
