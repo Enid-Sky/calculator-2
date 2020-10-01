45634296b4c642cfe4f442c0ee1ef7ac24ccf6bb
Enid-Sky <enid.sky@protonmail.com>
2020-09-30 18:50:48 -0700

    add error handling

--- calculator-2.py
+++ calculator-2.py
@@ -12,3 +12,18 @@ while True:
     operator = tokens[0]
     if operator == "q":
         break
+
+    # Checking for input of 1 and returning error
+    if len(tokens) < 2:
+        print('Invalid entry, please try again.')
+
+    # Assigning first input digit to var num1
+    num1 = tokens[1]
+    if not num1.isdigit():
+        print("Please provide a number")
+
+    # Checking if input is 3 or more
+    if len(tokens) > 2:
+        num2 = tokens[2]
+    if not num2.isdigit():
+        print("Please provide a number")
