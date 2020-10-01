bd977fca973649eced63532a5d7424118eedd1c1
Enid-Sky <enid.sky@protonmail.com>
2020-09-30 18:53:56 -0700

    call functions based on user input

--- calculator-2.py
+++ calculator-2.py
@@ -8,8 +8,8 @@ while True:
     input_string = input("Enter your equation: ")
     tokens = input_string.split(' ')
     result = None
-
     operator = tokens[0]
+
     if operator == "q":
         break
 
@@ -27,3 +27,31 @@ while True:
         num2 = tokens[2]
     if not num2.isdigit():
         print("Please provide a number")
+
+    # Based on the users input will call the necessary functions
+
+    if operator == "+":
+        result = add(float(num1), float(num2))
+
+    elif operator == "-":
+        result = subtract(float(num1), float(num2))
+
+    elif operator == "*":
+        result = multiply(float(num1), float(num2))
+
+    elif operator == "/":
+        result = divide(float(num1), float(num2))
+
+    elif operator == "square":
+        result = square(float(num1))
+
+    elif operator == "cube":
+        result = cube(float(num1))
+
+    elif operator == "power":
+        result = power(float(num1), float(num2))
+
+    elif operator == "mod":
+        result = mod(float(num1), float(num2))
+
+    print(result)
